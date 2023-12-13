d9607335a7bdfb2184039387b4ffebee35d6c53d
Patrick Lindsay <pat@thatdarnpat.com>
2023-12-13 10:53:44 -0600

    Add custom Kitty icon

--- darwin/files.nix
+++ darwin/files.nix
@@ -4,15 +4,14 @@ let
   xdg_configHome = "${config.users.users.${user}.home}/.config";
   xdg_dataHome   = "${config.users.users.${user}.home}/.local/share";
   xdg_stateHome  = "${config.users.users.${user}.home}/.local/state";
-  dotfiles = pkgs.fetchFromGitHub {
-    owner = "tlindsay";
-    repo = "dotfiles";
-    rev = "b0b41900d1385175ed594c3eb9a5c14842ddcb8b";
-    sha256 = null;
+  kitty_icon = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/igrmk/whiskers/main/whiskers.icns";
+    sha256 = "c5bff113eac01b0074cc862eca3380e3ceb835d28a43f207aa77a9820be3262e";
   };
-  in
+in
 {
   "darwin.txt" = {
     text = "hi, ${user}! this is from darwin/files.nix";
   };
+  "${xdg_configHome}/kitty/kitty.app.icns".source = kitty_icon;
 }
