39dad12507c840cab19b9f3dd6f2951d53f52455
Patrick Lindsay <pat@thatdarnpat.com>
2024-07-09 16:33:29 -0500

    Add impala WiFi TUI for linux

--- nixos/packages.nix
+++ nixos/packages.nix
@@ -1,73 +1,76 @@
-{ pkgs }:
-with pkgs;
-let shared-packages = import ../shared/packages.nix { inherit pkgs; };
-in shared-packages ++ [
-  # Security and authentication
-  yubikey-manager-qt
-  yubikey-agent
-  keepassxc
+{pkgs}:
+with pkgs; let
+  shared-packages = import ../shared/packages.nix {inherit pkgs;};
+in
+  shared-packages
+  ++ [
+    # Security and authentication
+    yubikey-manager-qt
+    yubikey-agent
+    keepassxc
 
-  # App and package management
-  appimage-run
-  gnumake
-  cmake
-  home-manager
+    # App and package management
+    appimage-run
+    gnumake
+    cmake
+    home-manager
 
-  # Media and design tools
-  vlc
-  fontconfig
-  font-manager
+    # Media and design tools
+    vlc
+    fontconfig
+    font-manager
 
-  # Calculators
-  bc # old school calculator
-  galculator
+    # Calculators
+    bc # old school calculator
+    galculator
 
-  # Audio tools
-  cava # Terminal audio visualizer
-  pavucontrol # Pulse audio controls
+    # Audio tools
+    cava # Terminal audio visualizer
+    pavucontrol # Pulse audio controls
 
-  # Messaging and chat applications
-  cider # Apple Music on Linux
-  # tdesktop # telegram desktop
+    # Messaging and chat applications
+    cider # Apple Music on Linux
+    # tdesktop # telegram desktop
 
-  # Testing and development tools
-  direnv
-  rofi
-  rofi-calc
-  rnix-lsp # lsp-mode for nix
-  postgresql
+    # Testing and development tools
+    direnv
+    rofi
+    rofi-calc
+    rnix-lsp # lsp-mode for nix
+    postgresql
 
-  # Screenshot and recording tools
-  flameshot
-  simplescreenrecorder
+    # Screenshot and recording tools
+    flameshot
+    simplescreenrecorder
 
-  # Text and terminal utilities
-  feh # Manage wallpapers
-  screenkey
-  tree
-  unixtools.ifconfig
-  unixtools.netstat
-  xorg.xwininfo # Provides a cursor to click and learn about windows
-  xorg.xrandr
+    # Text and terminal utilities
+    feh # Manage wallpapers
+    screenkey
+    tree
+    unixtools.ifconfig
+    unixtools.netstat
+    xorg.xwininfo # Provides a cursor to click and learn about windows
+    xorg.xrandr
+    impala # TUI for managing wifi
 
-  # File and system utilities
-  inotify-tools # inotifywait, inotifywatch - For file system events
-  i3lock-fancy-rapid
-  libnotify
-  playerctl # Control media players from command line
-  pinentry-curses
-  pcmanfm # Our file browser
-  sqlite
-  xdg-utils
+    # File and system utilities
+    inotify-tools # inotifywait, inotifywatch - For file system events
+    i3lock-fancy-rapid
+    libnotify
+    playerctl # Control media players from command line
+    pinentry-curses
+    pcmanfm # Our file browser
+    sqlite
+    xdg-utils
 
-  # Other utilities
-  yad # I use yad-calendar with polybar
-  xdotool
-  google-chrome
+    # Other utilities
+    yad # I use yad-calendar with polybar
+    xdotool
+    google-chrome
 
-  # PDF viewer
-  zathura
+    # PDF viewer
+    zathura
 
-  # Music and entertainment
-  spotify
-]
+    # Music and entertainment
+    spotify
+  ]
