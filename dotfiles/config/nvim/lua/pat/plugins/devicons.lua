60fc77badce2f4d1027f3b1c8ccd66a684460be8
Patrick Lindsay <plindsay@fastly.com>
2025-03-20 17:11:34 -0500

    Remove dead devicons config entry

--- dotfiles/config/nvim/lua/pat/plugins/devicons.lua
+++ dotfiles/config/nvim/lua/pat/plugins/devicons.lua
@@ -1,16 +1,9 @@
+local colors = require('pat.core.colors')
 require('nvim-web-devicons').setup({
   override = {
     nvim = {
       icon = '',
-      color = '#00b952',
-      cterm_color = '65',
-      name = 'Neovim',
-    },
-  },
-  override_by_filename = {
-    ['.config/nvim/**/*'] = {
-      icon = '',
-      color = '#00b952',
+      color = colors.green,
       cterm_color = '65',
       name = 'Neovim',
     },
