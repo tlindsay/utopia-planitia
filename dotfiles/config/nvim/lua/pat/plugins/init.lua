283f49cdeab47b28f21032624f7e59830db7cae9
Patrick Lindsay <plindsay@fastly.com>
2025-04-25 16:36:13 -0500

    Fix missing octo.nvim

--- dotfiles/config/nvim/lua/pat/plugins/init.lua
+++ dotfiles/config/nvim/lua/pat/plugins/init.lua
@@ -250,6 +250,14 @@ return {
       require('git-rebase-auto-diff').setup()
     end,
   },
+  {
+    'pwntester/octo.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+      'nvim-tree/nvim-web-devicons',
+    },
+  },
 
   -- git labels
   {
