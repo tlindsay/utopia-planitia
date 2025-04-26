58c0a07bf5fb0fce933105e5ad8f185ceca589aa
Patrick Lindsay <pat@thatdarnpat.com>
2025-04-26 15:31:23 -0500

    Improve linux-builder config

--- modules/darwin/nix/default.nix
+++ modules/darwin/nix/default.nix
@@ -16,7 +16,20 @@ in {
 
     package = pkgs.nixVersions.latest;
 
-    linux-builder.enable = true;
+    linux-builder = {
+      enable = true;
+      ephemeral = true;
+      maxJobs = 4;
+      config = {
+        virtualisation = {
+          darwin-builder = {
+            diskSize = 40 * 1024;
+            memorySize = 8 * 1024;
+          };
+          cores = 6;
+        };
+      };
+    };
 
     settings = {
       trusted-users = ["@admin" "${user}"];
