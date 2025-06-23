2b1aeb711fbd297a97f30560582d6690033607a2
Patrick Lindsay <plindsay@fastly.com>
2025-06-23 11:37:34 -0500

    Fix mismatched tailscale pkgs

--- modules/darwin/services/default.nix
+++ modules/darwin/services/default.nix
@@ -1,10 +1,16 @@
-{pkgs, ...}: {
+{
+  inputs,
+  system,
+  ...
+}: let
+  upkgs = inputs.nixpkgs-unstable.legacyPackages."${system}";
+in {
   services = {
     # Auto upgrade nix package and the daemon service.
     nix-daemon.enable = true;
     tailscale = {
       enable = true;
-      package = pkgs.tailscale;
+      package = upkgs.tailscale;
     };
   };
 }
